e648f264b989c5a9246ed8cd7dec9b70afa591d5
MrPowerGamerBR <leonardomalaman@gmail.com>
2019-04-16 22:19:05 -0300

    Embelezar tweets

--- src/main/kotlin/net/mrgaabriel/bomdiagenerator/manager/TwitterManager.kt
+++ src/main/kotlin/net/mrgaabriel/bomdiagenerator/manager/TwitterManager.kt
@@ -15,6 +15,13 @@ import javax.imageio.ImageIO
 import kotlin.concurrent.thread
 
 class TwitterManager(val config: BomDiaConfig) {
+    val randomEmotes = listOf(
+        "\uD83D\uDE0A",
+        "\uD83C\uDF1E",
+        "\uD83D\uDE42",
+        "\uD83D\uDC4D",
+        "\uD83D\uDC96"
+    )
 
     var twitter: Twitter
 
@@ -35,7 +42,7 @@ class TwitterManager(val config: BomDiaConfig) {
         ImageIO.write(image, "png", baos)
         val bais = ByteArrayInputStream(baos.toByteArray())
 
-        val status = twitter.updateStatus(StatusUpdate("Bom dia, grupo do zap! #GoodMorningWorld #GoodMorning #BomDia").media("bom-dia.png", bais))
+        val status = twitter.updateStatus(StatusUpdate("Bom dia, grupo do zap! ${randomEmotes.random()}\n\n#GoodMorningWorld #GoodMorning #BomDia").media("bom-dia.png", bais))
 
         println("Tweeted successfully! https://twitter.com/${twitter.screenName}/status/${twitter.id}")
         return status
